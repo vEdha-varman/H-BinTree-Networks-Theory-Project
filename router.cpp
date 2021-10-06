178f2b4903b480d05ea121b4d9948c8c38719f37
A S Vignesh <59014425+vEdha-varman@users.noreply.github.com>
2021-10-06 17:36:02 +0530

    strCmp() initial stage

--- router.cpp
+++ router.cpp
@@ -1,20 +1,50 @@
 // Inside Router
 #include <iostream>
+#include <string>
 // #include <bits/stdc++.h>
 
 using namespace std;
 
+bool strCmp(string a, string b)
+{
+	int aa = a.size(), bb = b.size();
+	// if (aa != bb)
+	// {
+	// 	return aa > bb;
+	// }
+	// else
+	{
+		int i;
+		for (i = 0; i < aa && i < bb; i++)
+		{
+			if (a[i] != b[i])
+			{
+				return a[i] > b[i];
+			}
+		}
+		if (aa!=bb)
+		{
+			if (aa == i)
+			{
+				/* code */
+			}
+			
+		}
+		
+	}
+	
+	
+}
+
 class BSTreeNode
 {
 public:
-	int prefix;
+	string prefix;
 	BSTreeNode *left;
 	BSTreeNode *right;
 	BSTreeNode *child;
 	BSTreeNode(/* args */){}
 };
-
-
 
 class BSTree
 {
@@ -31,6 +61,20 @@ public:
 
 int main()
 {
-	cout << "\n\nUnder construction..\n\n";
+	cout << "\n\nUnder construction..\n\n" << strCmp("1000", "101");
 	return 0;
 }
+
+
+
+
+
+
+/**
+ * first the strCmp() should be able to tell whether the first arg should be to left(0) or right(1) to the 2nd arg
+ * 
+ * then comes the tree alone
+ * 
+ * then the clients and servers with ip address kick in,
+ * followed by port(/ranges)
+ */
